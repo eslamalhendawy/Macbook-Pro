ecd9df7fd46d9322f49214eb3c337b2007465b20
Eslam Alhendawy <eslam_alhendawy@outlook.com>
2026-03-10 19:32:15 +0200

    Started working on features section.

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import clsx from "clsx";
+
+import StudioLights from "./three/StudioLights";
+
+import { features } from "../constants/index";
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <section id="features">
+      <h2>See it all in a new light.</h2>
+      <Canvas id="f-canvas" camera={{}}>
+        <StudioLights />
+        <ambientLight intensity={0.5} />
+      </Canvas>
+      <div className="absolute inset-0">
+        {features.map((feature, index) => (
+          <div key={index} className={clsx("box", `box${index + 1}`, feature.styles)}>
+            {feature.text}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Features
+export default Features;
